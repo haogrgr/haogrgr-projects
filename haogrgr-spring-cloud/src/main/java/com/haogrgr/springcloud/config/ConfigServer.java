5d732ea731bae4f59f6b5a49a27b01327e67beb3
haogrgr <haogrgr@126.com>
2015-03-08 00:37:52 +0800

    修改配置,默认使用Git仓库,设置为简单实现

--- haogrgr-spring-cloud/src/main/java/com/haogrgr/springcloud/config/ConfigServer.java
+++ haogrgr-spring-cloud/src/main/java/com/haogrgr/springcloud/config/ConfigServer.java
@@ -1,12 +1,26 @@
 package com.haogrgr.springcloud.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.EnvironmentRepository;
+import org.springframework.cloud.config.server.NativeEnvironmentRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
 public class ConfigServer {
+
+	@Autowired
+	private ConfigurableEnvironment environment;
+
+	@Bean
+	public EnvironmentRepository environmentRepository() {
+		NativeEnvironmentRepository r = new NativeEnvironmentRepository(environment);
+		return r;
+	}
 
 }
